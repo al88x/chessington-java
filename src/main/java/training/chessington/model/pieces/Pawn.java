6ad1dab3dc6acc1b1ff87a1748bfee556acc2af7
Alexandru Catarau <alexctrx@gmail.com>
2019-10-31 14:36:48 +0000

    Add "Pawn can move to squares from start position" tests

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -16,6 +16,8 @@ public class Pawn extends AbstractPiece {
 
     private static final int W_MOVE_FWD = -1;
     private static final int B_MOVE_FWD = 1;
+    private static final int W_MOVE_FWD_2 = -2;
+    private static final int B_MOVE_FWD_2 = 2;
 
     public Pawn(PlayerColour colour) {
         super(Piece.PieceType.PAWN, colour);
@@ -24,11 +26,17 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> availableMoves = new ArrayList<>();
-        if(this.colour == WHITE){
-            availableMoves.add(new Move(from, from.plus(W_MOVE_FWD,0)));
+        if (this.colour == WHITE) {
+            if (from.getRow() == 6) {
+                availableMoves.add(new Move(from, from.plus(W_MOVE_FWD_2, 0)));
+            }
+            availableMoves.add(new Move(from, from.plus(W_MOVE_FWD, 0)));
         }
-        if(this.colour == BLACK){
-            availableMoves.add(new Move(from, from.plus(B_MOVE_FWD,0)));
+        if (this.colour == BLACK) {
+            if (from.getRow() == 1) {
+                availableMoves.add(new Move(from, from.plus(B_MOVE_FWD_2, 0)));
+            }
+            availableMoves.add(new Move(from, from.plus(B_MOVE_FWD, 0)));
         }
         return availableMoves;
     }
