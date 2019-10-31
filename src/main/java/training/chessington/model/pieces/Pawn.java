0c9710d85d0ae587efbe2af69f1ef9fd2d1d16b3
Alexandru Catarau <alexctrx@gmail.com>
2019-10-31 14:25:13 +0000

    Add tests "pawn move one step forward"

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -5,16 +5,31 @@ import training.chessington.model.Coordinates;
 import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
 
+import javax.accessibility.AccessibleValue;
 import java.util.ArrayList;
 import java.util.List;
 
+import static training.chessington.model.PlayerColour.BLACK;
+import static training.chessington.model.PlayerColour.WHITE;
+
 public class Pawn extends AbstractPiece {
+
+    private static final int W_MOVE_FWD = -1;
+    private static final int B_MOVE_FWD = 1;
+
     public Pawn(PlayerColour colour) {
         super(Piece.PieceType.PAWN, colour);
     }
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> availableMoves = new ArrayList<>();
+        if(this.colour == WHITE){
+            availableMoves.add(new Move(from, from.plus(W_MOVE_FWD,0)));
+        }
+        if(this.colour == BLACK){
+            availableMoves.add(new Move(from, from.plus(B_MOVE_FWD,0)));
+        }
+        return availableMoves;
     }
 }
